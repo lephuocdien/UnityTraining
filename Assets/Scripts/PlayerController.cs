8857a371a9321e8f3a3566e43e8af761780dddfe
lephuocdien <ledientesttb@gmail.com>
2020-03-16 23:45:27 +0700

    +moving camera

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody rb ;
+    public float speed;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movehorizontal = Input.GetAxis("Horizontal");
+        float movevertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(movehorizontal,0.0f,movevertical);
+        rb.AddForce(movement * speed);
+    }
+}
