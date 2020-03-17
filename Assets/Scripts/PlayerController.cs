0abb82824e21b7d4f460c20b8b181a46ff31633e
lephuocdien <ledientesttb@gmail.com>
2020-03-17 23:14:46 +0700

    +Finish rool a ball

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     private Rigidbody rb ;
     public float speed;
+    private int count;
+    public Text counttext;
+    public Text wintext;
+    public 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        ShowSetCounttext();
+        wintext.text = "";
     }
 
     // Update is called once per frame
@@ -19,5 +26,23 @@ public class PlayerController : MonoBehaviour
         float movevertical = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(movehorizontal,0.0f,movevertical);
         rb.AddForce(movement * speed);
+       
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            ShowSetCounttext();
+        }
+    }
+    void ShowSetCounttext()
+    {
+        counttext.text = "Count:" + count;
+        if(count>=12)
+        {
+            wintext.text = "You Win!";
+        }
     }
 }
