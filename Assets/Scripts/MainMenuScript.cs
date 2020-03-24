2d9aa179c0bbfe26a9b6d22eeb4c885191fab297
lephuocdien <ledientesttb@gmail.com>
2020-03-24 15:47:37 +0700

    + mainmenu

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    // Start is called before the first frame update
+    public void GotoOption()
+    {
+
+    }
+    public void GotoPlay()
+    {
+
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
